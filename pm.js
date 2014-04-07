1031eed801af4fb719eb311ec7f99a318965c832
aerinzhang <shikunz@andrew.cmu.edu>
2014-04-07 14:18:12 -0400

    update existing

--- pm.js
+++ pm.js
@@ -165,8 +165,7 @@ function convertNestedStoriesToString(array) {
 //search the given element in a dropbox list
 function searchDropBoxList(list, element) {
 	for (var i=0; i<list.length; i++) {
-		if list.get(i) == element:
-			return i;
+		if (list.get(i) === element) return i;
 	}
 	return -1;
 }
