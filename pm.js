0712292ad2bfe1caf1b315a33d1302ee32d214d5
aerinzhang <shikunz@andrew.cmu.edu>
2014-04-28 13:34:16 -0400

    change it to li

--- pm.js
+++ pm.js
@@ -1077,8 +1077,8 @@ function renderStoryBank() {
 				var record = records[i];
 				var score = calculateScoreForStory(record);
 				//var li = '<li><a href="#" ><img src="images/person/{0}.jpg"><p>{1}</p></a></li>'
-				var pair = "<div><span class='pairdiv'><figure><img class=pair src=images/person/{0}.jpg /><figcaption>{1}</figcaption></figure> \
-						 	<figure><img class=pair src=images/scene/{2}.jpg /><figcaption>{3}</figcaption></figure></span>{4} Score : {5}</div>";
+				var pair = "<li><span class='pairdiv'><figure><img class=pair src=images/person/{0}.jpg /><figcaption>{1}</figcaption></figure> \
+						 	<figure><img class=pair src=images/scene/{2}.jpg /><figcaption>{3}</figcaption></figure></span>{4} Score : {5}</li>";
 				var newli = String.format(pair, record.get('person'), record.get('person'), record.get('scene').toLowerCase(), record.get('scene'), record.get('lastRehearsed').toString(), score.toString());
 	    		listHTML += newli;
 	    	}
