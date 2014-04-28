7c1f233b5a80d89c8b2e8ad5e942cd0aae9bd2eb
aerinzhang <shikunz@andrew.cmu.edu>
2014-04-28 18:22:23 -0400

    list style none

--- pm.js
+++ pm.js
@@ -1079,7 +1079,7 @@ function renderStoryBank() {
 				//var li = '<li><a href="#" ><img src="images/person/{0}.jpg"><p>{1}</p></a></li>'
 				var pair = "<li><span class='pairdiv'><figure><img class=pair src=images/person/{0}.jpg /><figcaption><span class='storyText'>{1}</span></figcaption></figure> \
 						 	<figure><img class=pair src=images/scene/{2}.jpg /><figcaption><span class='storyText'>{3}</span></figcaption></figure></span><span class='storyText'>{4}</span><span class='storyText'>Score : {5}</span></li>";
-				var newli = String.format(pair, record.get('person'), record.get('person').replace('_', ' '), record.get('scene').toLowerCase(), \
+				var newli = String.format(pair, record.get('person'), record.get('person').replace('_', ' '), record.get('scene').toLowerCase(),
 					record.get('scene').replace('_', ' '), record.get('lastRehearsed').toString(), score.toString());
 	    		listHTML += newli;
 	    	}
