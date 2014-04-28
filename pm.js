ca7328b8e8396f264210e2d248b90edc68ebc540
aerinzhang <shikunz@andrew.cmu.edu>
2014-04-28 18:21:00 -0400

    list style none

--- pm.js
+++ pm.js
@@ -1077,9 +1077,10 @@ function renderStoryBank() {
 				var record = records[i];
 				var score = calculateScoreForStory(record);
 				//var li = '<li><a href="#" ><img src="images/person/{0}.jpg"><p>{1}</p></a></li>'
-				var pair = "<li><span class='pairdiv'><figure><img class=pair src=images/person/{0}.jpg /><figcaption><p>{1}</p></figcaption></figure> \
-						 	<figure><img class=pair src=images/scene/{2}.jpg /><figcaption><p>{3}</p></figcaption></figure></span><p>{4}</p><p>Score : {5}</p></li>";
-				var newli = String.format(pair, record.get('person'), record.get('person'), record.get('scene').toLowerCase(), record.get('scene'), record.get('lastRehearsed').toString(), score.toString());
+				var pair = "<li><span class='pairdiv'><figure><img class=pair src=images/person/{0}.jpg /><figcaption><span class='storyText'>{1}</span></figcaption></figure> \
+						 	<figure><img class=pair src=images/scene/{2}.jpg /><figcaption><span class='storyText'>{3}</span></figcaption></figure></span><span class='storyText'>{4}</span><span class='storyText'>Score : {5}</span></li>";
+				var newli = String.format(pair, record.get('person'), record.get('person').replace('_', ' '), record.get('scene').toLowerCase(), \
+					record.get('scene').replace('_', ' '), record.get('lastRehearsed').toString(), score.toString());
 	    		listHTML += newli;
 	    	}
 	    	listHTML += "</ul></div>";
