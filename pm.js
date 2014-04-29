4b9ffe71cc6915401566e2faf9093955fa8c095a
aerinzhang <shikunz@andrew.cmu.edu>
2014-04-29 19:59:16 -0400

    add click li function

--- pm.js
+++ pm.js
@@ -288,8 +288,6 @@ function createPageForStory(person, scene) {
 		var newPage = $("<div data-role='page' data-title='"+person+scene+"' id="+person+scene+"Page><div data-role='header' data-position=fixed>\
 								<a href=#board data-icon='back'>Back</a><h1>"+ Rehearsal + "</h1></div><div data-role='content' class=images>"+pageHtml+" </div></div>");
 		
-	}
-
 }
 function renderRehearsalBoard() {
 	var html = "Welcome back!";
