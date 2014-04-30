10ec4a8b47753811d2f314d1d87be99a27f773b7
aerinzhang <shikunz@andrew.cmu.edu>
2014-04-29 20:12:26 -0400

    add click li function

--- pm.js
+++ pm.js
@@ -287,6 +287,7 @@ function createPageForStory(person, scene) {
 	var newPage = $("<div data-role='page' data-title='"+person+scene+"' id="+person+scene+"Page><div data-role='header' data-position=fixed>\
 					 <a href=#board data-icon='back'>Back</a><h1>Rehearsal</h1></div><div data-role='content' class=images>"+newHTML+" </div></div>");
 	$( "#"+person+scene ).page( "destroy" ).page();
+	newPage.appendTo( $.mobile.pageContainer );
 	$.mobile.changePage('#'+person+scene);
 	$('#rehearsal-password').focus();
 		
