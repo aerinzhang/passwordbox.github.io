efc521b48d8a1e2d8e1378ae55250cbda296cc4e
aerinzhang <shikunz@andrew.cmu.edu>
2014-09-22 12:32:36 -0700

    add images and debugging

---
+++ accountPage.js
@@ -0,0 +1,2 @@
+//this module is for 
+var accountPage = accountPage || {};
--- appConstants.js
+++ appConstants.js
@@ -2,43 +2,53 @@ var appConstants = appConstants || {};
 
 
 appConstants.NUM_OF_ROUNDS = 5;
-appConstants.peopleList = ["Adolf_Hitler","Angelina_Jolie", 'Alfred_Hitchcock', 'Audrey_Hepburn', 'Barack_Obama',"Bart_Simpson", 
-						  "Beethoven", "Ben_Affleck", "Benjamin_Franklin", "Beyonce", //10
-						  "Bill_Clinton",'Bill_Gates', "Brad_Pitt", "Bruce_Lee",
-						  "Charlie_Chaplin", "Christopher_Columbus", "Darth_Vader","David_Beckham", 'Einstein', "Elvis_Presley",//10
-						  'Frankenstein',"Frodo", "Gandalf", "Gandhi",
-						  "George_W_Bush", 'George_Washington', "Harry_Potter",
-						  "Hillary_Clinton", "Homer_Simpson", "Indiana_Jones", 
-						  "Jennifer_Lopez", "Jimmy_Fallon", //12
-						  "John_Lennon", "Johnny_Depp",
-						  "Justin_Timberlake", "Kim_Jong_Un", "Kobe_Bryant",
-						   "Lebron_James", "Leonardo_da_Vinci", "Leonardo_DiCaprio",
-						   "Madonna", "Marilyn_Monroe", //10
-						   "Mark_Twain",
-						   "Mark_Zuckerberg", "Martin_Luther_King_Jr",
-						   "Michael_Jackson","Michael_Jordan", "Michael_Phelps", 'Michelle_Obama', 
-						   'Mickey_Mouse', "Mona_Lisa", 'Morgan_Freeman','Mozart', //11
-						   "Neil_Armstrong", "Nelson_Mandela", 
-						   "Oprah_Winfrey",
-						   "Pope_Francis", "Princess_Diana", 'Ronald_Reagan', "Sherlock_Holmes",
-						   "Sir_Issac_Newton", "Stephen_Hawking", "Steve_Jobs", //10
-						   "Superman",
-						   "Thomas_Edison", "Tiger_Woods", "Vincent_Van_Gogh", "Vladimir_Putin", "William_Shakespeare"];
+appConstants.peopleList = [ "Adolf_Hitler", "Alfred_Hitchcock", "Angelina_Jolie", "Audrey_Hepburn", "Barack_Obama",
+							"Bart_Simpson", "Beethoven", "Ben_Affleck", "Benjamin_Franklin", "Beyonce", 
+							"Bill_Clinton", "Bill_Gates", "Brad_Pitt", "Bruce_Lee", "Charlie_Chaplin",
+							"Christopher_Columbus", "Darth_Vader", "David_Beckham", "Einstein", "Elvis_Presley",//20
+							"Frankenstein", "Frodo", "Gandalf", "Gandhi", "George_W_Bush",
+							"George_Washington", "Harry_Potter", "Hillary_Clinton", "Homer_Simpson", "Indiana_Jones",
+							"Jay-Z", "Jennifer_Lopez", "Jimmy_Fallon", "Joe_Biden", "John_Lennon",
+							"Johnny_Depp", "Justin_Timberlake", "Kim_Jong_Un", "Kobe_Bryant", "Lady_Gaga",//40
+							"Lebron_James", "Leonardo_da_Vinci", "Leonardo_DiCaprio", "Madonna", "Marilyn_Monroe", 
+							"Mark_Twain", "Mark_Zuckerberg", "Martin_Luther_King_Jr", "Michael_Jackson", "Michael_Jordan",
+							"Michael_Phelps", "Michelle_Obama", "Mickey_Mouse", "Mona_Lisa", 'Morgan_Freeman',
+							'Mozart', "Neil_Armstrong", "Nelson_Mandela", "Oprah_Winfrey", "Pope_Francis",//60
+							"Princess_Diana", "Rand_Paul", "Ron_Paul", "Ronald_Reagan", "Sherlock_Holmes",
+							"Sir_Issac_Newton", "Stephen_Hawking", "Steve_Jobs", "Superman", "Thomas_Edison", 
+							"Tiger_Woods", "Vincent_Van_Gogh", "Vladimir_Putin", "William_Shakespeare" //
+						   ]; //74
 
-appConstants.actionsList = ['balancing', 'bending', 'biting', 'bouncing', 'building', 'burning' , 'chasing', 'clapping', 'climbing' ,'cooking', 'digging',
-						   'drinking', 'enlarging', 'exploding', 'feeding', 'fighting', 'flipping', 'hanging', 'hiding', 'hugging', 'juggling', 'kissing',
-						   'licking', 'painting', 'piloting', 'pushing', 'repairing', 'rubbing', 'scratching', 'shooting', 'smelling', 'swinging','throwing',
-						   'tickling', 'tying', 'washing', 'wrapping', 'zooming'];
+appConstants.actionsList = [ 'balancing', 'bending', 'biting', 'bouncing', 'building', 
+							 'burning' , 'chasing', 'clapping', 'climbing' ,'cooking',
+							 'digging', 'drinking', 'enlarging', 'exploding', 'feeding',
+							 'fighting', 'flipping', 'gnawing', 'hanging', 'hiding', 
+							 'hugging', 'juggling', 'kissing', 'lassoing', 'licking',
+							 'oiling', 'painting', 'piloting', 'pushing', 'repairing',
+							 'rowing', 'rubbing', 'saving', 'scratching', 'signing',
+							 'sipping', 'shooting', 'stewing', 'smelling', 'swallowing',
+							 'swinging', 'taping', 'tattooing', 'throwing', 'tickling',
+							 'tugging', 'tying', 'washing', 'wrapping', 'zooming'
+							]; //#50
 
-appConstants.objectsList = ['dome','hammer','heel','hen','igloo','leaf', 'lock', 'moose', 'seal', 'smore', 'snowflake','suit','daisy','dice','safe','toilet',
-				  			'moon', 'map','lollipop','peach', 'bus'];
+appConstants.objectsList = [ 'ant', 'boot', 'bunny', 'bus', 'calf',
+							 'chandelier', 'cow', 'cupcake', 'daisy', 'dandelion',
+							 'dice', 'dome', 'dove', 'hammer', 'heel',
+							 'hen', 'hourglass', 'hydrant', 'igloo', 'ladder',
+							 'ladybug', 'leaf', 'lemon', 'lime', 'lipstick',
+							 'lock', 'lollipop', 'map', 'moon', 'moose',
+							 'owl', 'peach', 'piano', 'pizza', 'safe',
+							 'saw', 'seal', 'shark', 'shoe', 'smore',
+							 'snowflake', 'stapler', 'suit', 'sumo', 'teacup',
+							 'teepee', 'tiger', 'toaster', 'toilet', 'tricycle', 'violin']; //#51
 
-appConstants.scenesList = ['airport', 'baseball_field', 'basketball_court', 'bakery', 
-						  'Big_Ben','bridge', 'Capitol_Building', 'castle', 'cliff', 'clouds',
-						  'court', 'Eiffel_Tower', 'factory',
-						  'fancy_house', 'farm', 'fitness_center', 'forest', 'garden', 'garage',
-						  'Great_Sphynx', 'glacier', 'Grand_Canyon', 'Great_Wall', 'hanging_bridge', 
-						  'hotel_room','island', 'lake',
-						  'library', 'lighthouse', 'mountain', 'Niagara_Falls', 'ocean', 'office','pool_bar', 'pyramids', 'restaurant', 'Statue_of_Liberty', 
-						  'swimming_pool', 'Taj_Mahal',
-						  'Tower_of_Pisa', 'tropical_beach', 'wharf','windmills', 'zoo']; //42
+appConstants.scenesList = [ 'airport', 'aquarium', 'baseball_field', 'basketball_court', 'bakery',
+							'Big_Ben', 'bridge', 'Capitol_Hill', 'castle', 'cliff',
+							'clouds', 'court', 'Eiffel_Tower', 'factory', 'fancy_house',
+							'farm', 'fitness_center', 'forest', 'garden', 'garage',
+							'Great_Sphynx', 'glacier', 'Grand_Canyon', 'Great_Wall', 'hanging_bridge',
+							'hotel_room', 'island', 'lake', 'library', 'lighthouse',
+							'museum', 'mountain', 'Niagara_Falls', 'ocean', 'office',
+							'park', 'pool_bar', 'pyramids', 'restaurant', 'sailboat',
+							'snowy_mountain', 'Statue_of_Liberty', 'swimming_pool', 'Sydney_Opera_House', 'Taj_Mahal',
+							'Tower_of_Pisa', 'tropical_beach', 'waterfall', 'wharf', 'windmills', 'zoo']; //51
