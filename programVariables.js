cc2260b472d5921731f4829698ec70ed507afc57
aerinzhang <shikunz@andrew.cmu.edu>
2014-09-23 19:01:35 -0700

    testing if account is loaded

--- programVariables.js
+++ programVariables.js
@@ -46,7 +46,7 @@ programVariables.initialize = function (){
  		programVariables.datastore.recordsChanged.addListener(storyMode.updateStoryBankList);
 		programVariables.datastore.recordsChanged.addListener(accountPage.updateAccountList);	
 		alert('the end of initialization');
-		console.log(programVariables.accountTable.query());
+		console.log(programVariables.accountTable.query()[0].get('accountName'));
 		//UI Change after logging in REFER TO pm.js
 	});
 }
